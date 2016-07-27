e190a9bb4a49e65a578928bfa5d4ce3fe76a0d46
linzhixing <tomoyuki.hayashi@aegif.jp>
2016-07-27 20:43:34 +0900

    Externalize nemaki test parameters

--- core/src/test/java/jp/aegif/nemaki/test/nemaki/SessionUtil.java
+++ core/src/test/java/jp/aegif/nemaki/test/nemaki/SessionUtil.java
@@ -1,7 +1,10 @@
 package jp.aegif.nemaki.test.nemaki;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 import org.apache.chemistry.opencmis.client.api.OperationContext;
 import org.apache.chemistry.opencmis.client.api.Session;
@@ -13,13 +16,13 @@ import org.apache.chemistry.opencmis.commons.enums.IncludeRelationships;
 
 public class SessionUtil {
 	
-	public static Session createCmisSession(String repositoryId, String userId, String password){
+	public static Session createCmisSession(String repositoryId, String userId, String password) throws Exception{
+		Properties testConfig = readProperties();
+		
 		Map<String, String> parameter = new HashMap<String, String>();
-
 		// user credentials
-		//TODO enable change a user
-		parameter.put(SessionParameter.USER, userId);
-		parameter.put(SessionParameter.PASSWORD, password);
+		parameter.put(SessionParameter.USER, testConfig.getProperty("server.auth.user"));
+		parameter.put(SessionParameter.PASSWORD, testConfig.getProperty("server.auth.password"));
 
 		// session locale
 		parameter.put(SessionParameter.LOCALE_ISO3166_COUNTRY, "");
@@ -31,8 +34,8 @@ public class SessionUtil {
 
 		parameter. put(SessionParameter.BINDING_TYPE, BindingType.ATOMPUB.value());
 
-		String coreAtomUri = "http://localhost:8080/core/atom/" + repositoryId; //TODO
-		parameter.put(SessionParameter.ATOMPUB_URL, coreAtomUri);
+		String atompubUri = atompubUri(testConfig);
+		parameter.put(SessionParameter.ATOMPUB_URL, atompubUri);
 		
 		//timeout
 		//parameter.put(SessionParameter.CONNECT_TIMEOUT, "30000");
@@ -46,8 +49,26 @@ public class SessionUtil {
     	OperationContext operationContext = session.createOperationContext(null,
 				true, true, false, IncludeRelationships.BOTH, null, false, null, true, 100);
 		session.setDefaultContext(operationContext);
-
 		
 		return session;
+	}
+	
+	private static Properties readProperties() throws Exception{
+		Properties properties = new Properties();
+
+        String file = "neamki-test-parameters.properties";
+        InputStream inputStream = new FileInputStream(file);
+        properties.load(inputStream);
+        return properties;
+	}
+	
+	private static String atompubUri(Properties testConfig){
+		String server = testConfig.getProperty("server.endpoint.atompub");
+		String repository = testConfig.getProperty("server.endpoint.repository");
+		
+		if(!server.endsWith("/")){
+			server += "/";
+		}
+		return server + repository;
 	}
 }
